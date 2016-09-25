9d428635a832a0576a0d8fe3a5a5590df7e2e588
kingsir <jinminghao@ipaynow.cn>
2016-09-25 14:41:38 +0800

    base exception

---
+++ src/main/java/org/kin/live/live_account/except/BaseException.java
@@ -0,0 +1,15 @@
+package org.kin.live.live_account.except;
+
+/**
+ * Created by kingsir on 16-9-25.
+ */
+public class BaseException extends Exception{
+    public BaseException() {
+        super();
+    }
+    public BaseException(String message){
+        super(message);
+    }
+
+
+}
